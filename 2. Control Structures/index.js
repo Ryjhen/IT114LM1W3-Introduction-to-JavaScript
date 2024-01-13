decca99680aa419b02cc02f77587e4708ad87c7e
Ryjhen <ryjhen.shieda@gmail.com>
2024-01-13 17:59:50 +0800

    added partial 2 and 3

--- 2. Control Structures/index.js
+++ 2. Control Structures/index.js
@@ -8,38 +8,102 @@ const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock
 // TODO 2.1 Display "more than fifty" if randomNumber is more than fify
 // Your code here
 
+const statement1="more than fifty";
+const statement2="less than fifty";
+if (randomNumber >= 50) {
+    console.log(statement1);
+  } else {
+    console.log(statement2);
+  }
+
+
 // TODO 2.2 Display whether the random number is odd or even
 // Your code here
+
+
+
+if (randomNumber % 2) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+
+
 
 // TODO 2.3 If the number if a multiple of 3, write "fizz".
 // if the number is a multiple of 5 display "buzz".
 // if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
 // Your code here
 
+if (randomNumber % 3 ===0 && randomNumber % 5===0) {
+    console.log("fizz");
+} else if (randomNumber % 5 ===0) {
+    console.log("buzz");
+} else if (randomNumber % 3 ===0){
+    console.log("fizzbuzz");
+} else{
+    console.log(randomNumber)
+}
+
 let toDisplay = ""
 // TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
 // Your code here
-console.log("toDisplay", toDisplay);
+
+
+
 
 // Checkpoint 2.1 How do you use switch statements and when would you use them? Try researching the answer
 // Answer: 
+//you would use switch statements like an if else substitute. Wherein you would evaluate an expression's value to a series of clauses and 
+//executes statements involving them.
 
 // TODO 2.5 Use a for loop to print the numbers 1 to N
 const n = 10
-// Your code here
+// Your code herelet str = '';
+
+let str = '';
+for (let i = 0; i < 10; i++) {
+    str = str + i;
+  }
+  
+  console.log(str + n);
 
 const list = ["apple", "banana", "cherry", "date", "elderberry"];
 // TODO 2.6 Use a while loop to display all the values in the list
 // Your code here
 
+let i = 0;
+
+while (i < list.length) {
+    console.log(list[i]);
+    i++;
+}
+
+
 // Checkpoint 2.3 What is the difference between do while and while loop?
 // Answer: 
+//while: The condition is checked before executing any statements.
+//do-while: Statement is ran once and then checks the condition for further iterations.
 
 // TODO 2.7 Use a for of loop to display all the values in the list
 // Your code here
+console.log("--------Separate content--------")
+
+for (const element of list) {
+  console.log(element);
+}
 
 // TODO 2.8 Use a for in loop to display all the values in the list
 // Your code here
+
+console.log("--------Separate content--------")
+
+let txt = "";
+for (let x in list) {
+  txt += list[x] + " ";
+  console.log(txt);
+}
+
 
 // TODO 2.9 Use the for each method of the list to display all its values 
 // Your code here
@@ -49,15 +113,16 @@ const list = ["apple", "banana", "cherry", "date", "elderberry"];
 
 // TODO 2.10 Use the try and catch block to catch division by zero errors in the code below.
 // In the finally block, simulate cleaning up resources by displaying "cleaning up resources"
-const numerator = Math.floor((Math.random() * 100) + 1);
-const denominator = Math.floor((Math.random() * 5)); // feel free to mock this value for testing
+
+// const numerator = Math.floor((Math.random() * 100) + 1);
+// const denominator = Math.floor((Math.random() * 5)); // feel free to mock this value for testing
 
 
-if(denominator === 0){
-    throw new Error("Division by zero error")
-} else {
-    console.log(numerator / denominator);
-}
+// if(denominator === 0){
+//     throw new Error("Division by zero error")
+// } else {
+//     console.log(numerator / denominator);
+// }
 
 
 
--- 3. Arrays and Objects/index.js
+++ 3. Arrays and Objects/index.js
@@ -14,21 +14,72 @@ let numbers = [
 
 // Todo 3.1 Get the first, 5th, and last items in the numbers array.
 // Your code here
+const [firstItem, ...rest] = numbers;
+const lastItem = rest.pop();
+
+
+console.log('First item:', firstItem);
+console.log(numbers[4]);
+console.log('Last item:', lastItem);
+
 
 // Todo 3.2 calculate the min, max, and the average of the numbers array
 // Your code here
 
+
+ 
+    let minValue = Math.min(...numbers);
+    let maxValue = Math.max(...numbers);
+     
+    console.log("Minimum element is:" + minValue);
+    console.log("Maximum Element is:" + maxValue);
+
 // Checkpoint 3.1 How would you compare lists in python vs arrays in JavaScript
 // Answer: 
 
+//python: you would compare lists in python by using the sort method or the sorted function.
+//in other words, python already has a built in script for sorting.
+//javascript: you would compare lists in java by reference of their variables and would have to 
+// create your own custom script in doing so.
+
+
+
 // Todo 3.3 Declare an object with information about IT114L (course code, name, units, number of students)
 // Your code here
+const IT114L = {
+    courseCode: "IT114L",
+    name: "Christopher Kyle V. Santos",
+    units: 2,
+    numberOfStudents: 39,
+    profName: "Job Lipat"
+  };
 
 // Todo 3.4 Add professor name as one of the fields of the object. Display the value of professor name.
 // Your code here
+console.log(IT114L.profName);
+
 
 // Todo 3.5 Declare and array of objects with information about the courses you are taking this term
 // Your code here
+
+const courses = [ IT114L = {
+    courseCode: "IT114L",
+    name: "Christopher Kyle V. Santos",
+    units: 2,
+    numberOfStudents: 39,
+    profName: "Job Lipat"
+  },
+
+  CS107L = {
+    courseCode: "CS107L",
+    name: "Christopher Kyle V. Santos",
+    units: 2,
+    numberOfStudents: 39,
+    profName: "Sir Adomar Ilao"
+  },
+
+
+]
 
 // Todo 3.5 Calculate the total number of units you are taking this term using the array of objects.
 // Your code here
